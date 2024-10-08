ab807a8e26ba69cf50d71ace4b67e3335473a90e
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-08 20:25:20 +0100

    debut de la construction du front

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Le projet</div>;
+  return (
+    <div className="App">
+      <form>
+        <h3>Login</h3>
+        <div className="input">
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder="esubetabiseelelwa@gmail.com" />
+        </div>
+        <div className="input">
+          <label htmlFor="">Mot de passe</label>
+          <input type="password" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
