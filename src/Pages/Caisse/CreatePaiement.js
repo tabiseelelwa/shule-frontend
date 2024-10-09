3725282a02d0de73d47624b54ea7973432b8a273
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 22:44:42 +0100

    jjjjfjgfh

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {FaUser} from "react-icons/fa"
 
 const CreatePaiement = () => {
   return (
     <div className="createPaiement">
       <div className="createPaieHead">
-        <div className="userData"> <FaUser/> </div>
+        <div className="userData">
+          <div className="img"></div>
+        </div>
         <div className="otherData"></div>
         <div className="btnAjout"></div>
       </div>
