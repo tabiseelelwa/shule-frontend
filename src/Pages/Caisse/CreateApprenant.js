89a67ba911e869a8cf33483099823430a4841146
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 22:19:21 +0100

    interfaces de la caisse

---
+++ src/Pages/Caisse/CreateApprenant.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateApprenant = () => {
+  return <div>CreateApprenant</div>;
+};
+
+export default CreateApprenant;
---
+++ src/Pages/Caisse/CreatePaiement.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreatePaiement = () => {
+  return <div>CreatePaiement</div>;
+};
+
+export default CreatePaiement;
