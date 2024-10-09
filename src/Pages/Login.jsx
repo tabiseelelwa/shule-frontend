c21c3781c788de56d6f7cc8c60eb0761ce359e79
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 09:59:32 +0100

    ghfsghdfhj

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+export default function Login() {
+  return (
+    <div className="Login">
+      <form>
+        <div className="input">
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder="Ex: esubetabiseelelwa@gmail.com" />
+        </div>
+        <div className="input">
+          <label htmlFor="">Mot de passe</label>
+          <input type="password" placeholder="Entrez le mot de passe" />
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/App.scss";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Login from "./Pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element : <Login/>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <form>
-        <div className="input">
-          <label htmlFor="">Email</label>
-          <input type="text" placeholder="Ex: esubetabiseelelwa@gmail.com" />
-        </div>
-        <div className="input">
-          <label htmlFor="">Mot de passe</label>
-          <input type="password" placeholder="Entrez le mot de passe"/>
-        </div>
-      </form>
+    <div>
+      <div>
+        <RouterProvider router={router}/>
+      </div>
     </div>
   );
 }
