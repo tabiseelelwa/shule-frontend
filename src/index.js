7c302944069b8c3c1398c501888a9d7aea5bbf4f
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 22:37:05 +0100

    hfdhjf

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/App.scss";
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Pages/Login";
 import CreatePaiement from "./Pages/Caisse/CreatePaiement";
 import CreateApprenant from "./Pages/Caisse/CreateApprenant";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/App.scss";
+import "./style/paiement.scss";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element : <Login/>
+    element: <Login />,
   },
   {
     path: "/creat-paie",
-    element : <CreatePaiement/>
+    element: <CreatePaiement />,
   },
   {
     path: "/create-apprenant",
-    element : <CreateApprenant/>
+    element: <CreateApprenant />,
   },
-])
+]);
 
 function App() {
   return (
     <div>
       <div>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router} />
       </div>
     </div>
   );
