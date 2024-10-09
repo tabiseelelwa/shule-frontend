85c4af24c373e9e4b3994ee9b497ec348dbc468b
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 22:29:53 +0100

    hjdfhjdf

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -3,16 +3,10 @@ import React from "react";
 const CreatePaiement = () => {
   return (
     <div>
-      <div className="head">
-        <div className="userData">
-
-        </div>
-        <div className="otherData">
-
-        </div>
-        <div className="btnAjout">
-            
-        </div>
+      <div className="createPaieHead">
+        <div className="userData"></div>
+        <div className="otherData"></div>
+        <div className="btnAjout"></div>
       </div>
     </div>
   );
