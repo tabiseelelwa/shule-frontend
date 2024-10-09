f11685ba74f7c3dd2cc1e900ae1a52f817ed8d63
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-09 22:28:57 +0100

    hfhjfj

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 const CreatePaiement = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <div className="head">
+        <div className="userData">
+
+        </div>
+        <div className="otherData">
+
+        </div>
+        <div className="btnAjout">
+            
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePaiement;
