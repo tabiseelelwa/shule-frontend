6e470f35bec1609c635468bd20168da420291fe4
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-10 21:51:03 +0100

    hfh

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -20,7 +20,7 @@ const CreatePaiement = () => {
           <div className="asideButtons">
             <div className="button">Tous les Paiements</div>
             <div className="button">Modifier un paiement</div>
-            <div className="button"></div>
+            <div className="button">Liste de paiements du mois</div>
             <div className="button"></div>
           </div>
         </div>
