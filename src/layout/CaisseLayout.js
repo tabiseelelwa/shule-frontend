23df8fd49adc09f388de93853f53906f2a5ea15e
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-12 14:36:27 +0100

    Organisation des fichiers dans des layout

--- src/layout/CaisseLayout.js
+++ src/layout/CaisseLayout.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const CaisseLayout = () => {
   return (
-    <div>
+    <div className="caisse">
+      <div className="caisseHead">
+        <div className="userData">
+          <div className="img"></div>
+        </div>
+        <div className="otherData"></div>
+        <div className="btnAjout">
+          <Link></Link>
+        </div>
+      </div>
+      <div className="caisseBody">
+        <div className="aside">
+          <div className="asideTitle">
+            <h5>CAISSE</h5>
+          </div>
+          <div className="asideButtons">
+            <div className="button">
+              <Link>Apprenants</Link>
+            </div>
+            <div className="button">
+              <Link>Paiements</Link>
+            </div>
+            <div className="button">
+              <Link>Profil</Link>
+            </div>
+            <div className="button">Déconnexion</div>
+          </div>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
