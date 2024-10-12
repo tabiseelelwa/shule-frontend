6b66d6f014a3d9737cfafef57528be1112db0b5b
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-12 14:17:56 +0100

    hjf

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/App.scss";
 import "./style/paiement.scss";
 import App from "./App";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
