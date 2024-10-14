985565d224c00a21d60a524b0dbf6f546209484b
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-14 21:13:06 +0100

    aménagement de la partie contenu

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -1,9 +1,80 @@
 import React from "react";
-
+import { FaEdit, FaTrash } from "react-icons/fa";
 const CreatePaiement = () => {
   return (
     <div>
-      <p>CreatePAiement</p>
+      <div className="head">
+        
+      </div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Montant</th>
+            <th>Motif</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>12/10/2024</td>
+            <td>TABISEELELWA</td>
+            <td>Léon</td>
+            <td>350</td>
+            <td>Inscription</td>
+            <td>
+              <FaEdit style={{ color: "blue" }} />
+              <FaTrash style={{ color: "red" }} />
+            </td>
+          </tr>
+          <tr>
+            <td>12/10/2024</td>
+            <td>TABISEELELWA</td>
+            <td>Léon</td>
+            <td>150</td>
+            <td>Minerval 1ère Tr.</td>
+            <td>
+              <FaEdit style={{ color: "blue" }} />
+              <FaTrash style={{ color: "red" }} />
+            </td>
+          </tr>
+          <tr>
+            <td>12/10/2024</td>
+            <td>TABISEELELWA</td>
+            <td>Léon</td>
+            <td>150</td>
+            <td>Minerval 2ème Tr.</td>
+            <td>
+              <FaEdit style={{ color: "blue" }} />
+              <FaTrash style={{ color: "red" }} />
+            </td>
+          </tr>
+          <tr>
+            <td>12/10/2024</td>
+            <td>TABISEELELWA</td>
+            <td>Léon</td>
+            <td>150</td>
+            <td>Minerval 2ème Tr.</td>
+            <td>
+              <FaEdit style={{ color: "blue" }} />
+              <FaTrash style={{ color: "red" }} />
+            </td>
+          </tr>
+          <tr>
+            <td>12/10/2024</td>
+            <td>TABISEELELWA</td>
+            <td>Léon</td>
+            <td>150</td>
+            <td>Minerval 2ème Tr.</td>
+            <td>
+              <FaEdit style={{ color: "blue" }} />
+              <FaTrash style={{ color: "red" }} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
