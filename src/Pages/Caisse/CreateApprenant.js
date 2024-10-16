3fb8fcd625ed219f8f1b123d8d548cfd5ac6cd81
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-16 19:14:20 +0100

    finalisation du formulaire d'enregistrement de l'élève

--- src/Pages/Caisse/CreateApprenant.js
+++ src/Pages/Caisse/CreateApprenant.js
@@ -6,33 +6,36 @@ const CreateApprenant = () => {
       <div className="formHead">
         <p>CREATION ELEVE</p>
       </div>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <select name="">
-          <option>Choisir le sexe</option>
-          <option value="Masculin">Masculin</option>
-          <option value="Féminin">Féminin</option>
-        </select>
-        <input type="text" />
-        <select name="" id="">
-          <option>Choisir la section</option>
-          <option value="">Scientifique</option>
-          <option value="">Commerciale et gestion</option>
-          <option value="">Pédagogie générale</option>
-        </select>
-        <select name="" id="">
-          <option>Choisir la classe</option>
-          <option value="">1ère A</option>
-          <option value="">1ère B</option>
-          <option value="">2ème A</option>
-          <option value="">2ème B</option>
-          <option value="">2ème C</option>
-        </select>
-        <input type="text" />
-        <input type="text" />
-      </form>
+      <div className="formBody">
+        <form>
+          <input type="text" placeholder="Nom de l'élève" />
+          <input type="text" placeholder="Postnom de l'élève" />
+          <input type="text" placeholder="Prénom de l'élève" />
+          <select name="">
+            <option>Choisir le sexe</option>
+            <option value="Masculin">Masculin</option>
+            <option value="Féminin">Féminin</option>
+          </select>
+          <input type="text" placeholder="Adresse de résidence" />
+          <select name="" id="">
+            <option>Choisir la section</option>
+            <option value="">Scientifique</option>
+            <option value="">Commerciale et gestion</option>
+            <option value="">Pédagogie générale</option>
+          </select>
+          <select name="" id="">
+            <option>Choisir la classe</option>
+            <option value="">1ère A</option>
+            <option value="">1ère B</option>
+            <option value="">2ème A</option>
+            <option value="">2ème B</option>
+            <option value="">2ème C</option>
+          </select>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Téléphone" />
+          <button>Enregistrer</button>
+        </form>
+      </div>
     </div>
   );
 };
