933baaa8eed9b15b24a83bdbec96328d05c2404e
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-16 19:44:32 +0100

    finalisation form. enreg. eleve

--- src/Pages/Caisse/CreateApprenant.js
+++ src/Pages/Caisse/CreateApprenant.js
@@ -4,7 +4,7 @@ const CreateApprenant = () => {
   return (
     <div className="formulaire">
       <div className="formHead">
-        <p>CREATION ELEVE</p>
+        <p>ENREGISTREMENT ELEVE</p>
       </div>
       <div className="formBody">
         <form>
@@ -17,21 +17,6 @@ const CreateApprenant = () => {
             <option value="Féminin">Féminin</option>
           </select>
           <input type="text" placeholder="Adresse de résidence" />
-          <select name="" id="">
-            <option>Choisir la section</option>
-            <option value="">Scientifique</option>
-            <option value="">Commerciale et gestion</option>
-            <option value="">Pédagogie générale</option>
-          </select>
-          <select name="" id="">
-            <option>Choisir la classe</option>
-            <option value="">1ère A</option>
-            <option value="">1ère B</option>
-            <option value="">2ème A</option>
-            <option value="">2ème B</option>
-            <option value="">2ème C</option>
-          </select>
-          <input type="text" placeholder="Email" />
           <input type="text" placeholder="Téléphone" />
           <button>Enregistrer</button>
         </form>
