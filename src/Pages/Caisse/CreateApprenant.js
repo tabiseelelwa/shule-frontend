30a8edaca0ebea100f8aa2a258ee313e343eef03
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-16 12:35:10 +0100

    Le formulaire d'enregistrement des élèves

--- src/Pages/Caisse/CreateApprenant.js
+++ src/Pages/Caisse/CreateApprenant.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 const CreateApprenant = () => {
-  return <div>CreateApprenant</div>;
+  return (
+    <div>
+      <div className="formHead">
+        <p>CREATION ELEVE</p>
+      </div>
+      <form>
+        <input type="text" placeholder="Nom de l'élève" />
+        <input type="text" placeholder="Postnom de l'élève" />
+        <input type="text" placeholder="Prénom de l'élève" />
+        <select name="">
+          <option>Choisir le sexe</option>
+          <option value="Masculin">Masculin</option>
+          <option value="Féminin">Féminin</option>
+        </select>
+        <input type="text" placeholder="Adresse de résidence" />
+        <select name="" id="">
+          <option>Choisir la section</option>
+          <option value="">Scientifique</option>
+          <option value="">Commerciale et gestion</option>
+          <option value="">Pédagogie générale</option>
+        </select>
+        <select name="" id="">
+          <option>Choisir la classe</option>
+          <option value="">1ère A</option>
+          <option value="">1ère B</option>
+          <option value="">2ème A</option>
+          <option value="">2ème B</option>
+          <option value="">2ème C</option>
+        </select>
+        <input type="text" placeholder="" />
+      </form>
+    </div>
+  );
 };
 
 export default CreateApprenant;
