8492c72d76f121204c167e451c93f9f3b613d9cf
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-17 17:13:30 +0100

    form. paiement

--- src/Pages/Caisse/CreatePaiement.js
+++ src/Pages/Caisse/CreatePaiement.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const CreatePaiement = () => {
   return (
-    <div>CreatePaiement</div>
-  )
-}
+    <div className="formulaire">
+      <div className="formHead">
+        <p>NOUVEAU PAIEMENT</p>
+      </div>
+      <div className="formBody">
+        <form>
+          <select name="">
+            <option>Choisir l'élève</option>
+            <option value="Masculin">TABISEELELWA ESUBE Léon</option>
+            <option value="Féminin">ASENDE UNDJI Happy</option>
+          </select>
+          <select name="">
+            <option>Type de frais</option>
+            <option value="Masculin">Inscription</option>
+            <option value="Féminin">Minerval 1ère T.</option>
+            <option value="Féminin">Minerval 2ème T.</option>
+            <option value="Féminin">Minerval 3ème T.</option>
+            <option value="Féminin">Frais admin</option>
+            <option value="Féminin">Frais techniques</option>
+          </select>
+          <input type="number" placeholder="Montant payé" />
+          <button>Effectuer</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePaiement
+export default CreatePaiement;
