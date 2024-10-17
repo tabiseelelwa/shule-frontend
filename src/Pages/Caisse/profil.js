07a787c654439a21c5c285f6dd853ee89eba3131
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-17 17:28:30 +0100

    le profil de la caisse

--- src/Pages/Caisse/profil.js
+++ src/Pages/Caisse/profil.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import image from "./profil/image.jpg";
 const Profil = () => {
-  return <div>profil</div>;
+  return (
+    <div className="profil">
+      <div className="img">
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Profil;
