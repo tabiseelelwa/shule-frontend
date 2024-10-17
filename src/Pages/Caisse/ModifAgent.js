ce102ceb9be1a38ca227e5fbc3062edf55d20a2b
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-17 18:40:55 +0100

    creation des pages de modification agent de la caisse et eleve

---
+++ src/Pages/Caisse/ModifAgent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModifAgent = () => {
+  return <div>ModifAgent</div>;
+};
+
+export default ModifAgent;
---
+++ src/Pages/Caisse/ModifEleve.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModifEleve = () => {
+  return <div>ModifEleve</div>;
+};
+
+export default ModifEleve;
